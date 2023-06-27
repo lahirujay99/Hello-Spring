1e24143a333320e35238a5fe6a0f2258985fa78d
lahirujay99 <lahirujayawardana94@gmail.com>
2023-06-27 22:55:35 +0530

    add static html page for initial response

---
+++ src/main/java/com/lahiru/hellospring/HelloSpringApplication.java
@@ -0,0 +1,19 @@
+package com.lahiru.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
+
+
+//mvnw spring-boot:run
+//mvnw clean spring-boot:run
+
